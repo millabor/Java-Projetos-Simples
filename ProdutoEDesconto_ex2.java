9a56b74b888cac70a5f1feca40388afffb4e8c35
Milla Borges <107623760+millabor@users.noreply.github.com>
2023-03-11 09:03:22 -0300

    Add files via upload

---
+++ ProdutoEDesconto_ex2.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+public class ProdutoEDesconto_ex2 {
+    public static void main(String[] args){
+        double PrecoProduto = Double.parseDouble(JOptionPane.showInputDialog("Insira o preço do produto: "));
+        double ValorDesconto = Double.parseDouble(JOptionPane.showInputDialog("Insira o valor do desconto: "));
+        double Desconto = (ValorDesconto/100) * PrecoProduto;
+        double precoFinal = PrecoProduto - Desconto;
+
+        JOptionPane.showMessageDialog(null, "O preço final do produto é: " + precoFinal);
+
+    
+    }
+}
